8c9b63c2c1ccc6d0506b9393bb4a69224f1453d5
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-02-06 17:01:51 +0330

    connect blog single to blog home

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,11 @@ def blog_view(request):
     posts = Post.objects.all()
     context = {'posts':posts}
     return render(request ,'blog/blog-home.html' , context )
-def blog_single(request):
-    return render(request ,'blog/blog-single.html' )
+def blog_single(request , pid):
+    post = get_object_or_404(Post , pk=pid , status =1)
+    context = {'post':post}
+    return render(request ,'blog/blog-single.html',context )
+
 def test(request , pid):
     # post = Post.objects.get(id=pid)
     post = get_object_or_404(Post , pk=pid)
