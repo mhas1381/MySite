c10bb55d9ad200db579a2020097ad39204a644b6
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-02-08 16:16:53 +0330

    add categroy to url

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
+from multiprocessing import context
 from operator import ge
-from unicodedata import name
+from unicodedata import category, name
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 # Create your views here.
@@ -16,7 +17,13 @@ def blog_single(request , pid):
 # def test(request , pid):
 #     # post = Post.objects.get(id=pid)
 #     post = get_object_or_404(Post , pk=pid)
-#     context = {'ppst':post}
+#     context = {'post':post}
 #     return render(request , 'test.html' , context)
 def test(request):
     return render(request , 'test.html')
+
+def blog_category(request , cat_name):
+    posts = Post.objects.filter(status = 1)
+    posts = Post.objects.filter(category__name = cat_name)
+    context = {'posts':posts}
+    return render(request ,'blog/blog-home.html' , context)
