0bb2f1d8fa4908076feb9b467621578aae12fbbe
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-02-08 14:47:03 +0330

    creating template tag for total posts and title of the posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,13 @@
 from django import template
-
+from blog.models import Post
 register = template.Library()
+
+@register.simple_tag(name='postsCount')
+def function():
+    posts = Post.objects.filter(status = 1).count()
+    return posts
+
+@register.simple_tag(name='postTitles')
+def function():
+    posts = Post.objects.filter(status = 1)
+    return posts
