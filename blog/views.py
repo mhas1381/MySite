511e5b2c730c9cab1c54f68b81ad9295c18ac1c6
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-02-08 19:06:47 +0330

    bug in showing not published posts

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from blog.models import Post
 # Create your views here.
 
 def blog_view(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(status =1)
     context = {'posts':posts}
     return render(request ,'blog/blog-home.html' , context )
 def blog_single(request , pid):
