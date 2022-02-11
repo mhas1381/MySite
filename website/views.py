8999b8e804a3da478fe4aa4cadab36f5c64d5cc9
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-02-11 21:23:02 +0330

    creating test form for contact

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 import re
+from unicodedata import name
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from website.models import Contact
 def index_view(request):
     return render(request , 'website/index.html')
 
@@ -10,3 +11,18 @@ def about_view(request):
 
 def contact_view(request):
     return render(request , "website/contact.html")
+
+def test_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        c = Contact()
+        c.name = name
+        c.email = email
+        c.subject = subject
+        c.message = message
+        c.save()
+
+    return render(request ,'test.html')
