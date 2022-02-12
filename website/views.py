4b39ad245f747fe54bab5d520226e9317ddaa8fa
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-02-12 12:06:19 +0330

    using clean date in form

--- website/views.py
+++ website/views.py
@@ -1,5 +1,3 @@
-import re
-from unicodedata import name
 from django.shortcuts import render
 from django.http import HttpResponse
 from website.models import Contact
@@ -17,6 +15,11 @@ def test_view(request):
     if request.method == 'POST':
         form = NameForm(request.POST)
         if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            print(name,email,subject,message)
             return HttpResponse('done')
         else:
             return HttpResponse('not valid')
