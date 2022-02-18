7d74b502b069091127609c86f8812861535e134f
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-02-18 10:25:55 +0330

    adding sitemap to project

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from email.policy import default
 from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
@@ -26,3 +27,5 @@ class Post(models.Model):
         ordering = ['-created_date']
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:single' , kwargs={'pid':self.id})
