59444c1f4dea780edfc6744c1e2a05080524093f
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-02-18 12:43:59 +0330

    add tag to project

--- MySite/settings.py
+++ MySite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sitemaps',
     'robots',
     'debug_toolbar',
+    'taggit',
     'website',
     'blog'
 ]
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 # Create your models here.
 
 class Category(models.Model):
@@ -16,7 +17,7 @@ class Post(models.Model):
     author = models.ForeignKey(User , on_delete=models.SET_NULL , null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    # tag
+    tags = TaggableManager()
     category = models.ManyToManyField(Category)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
