6489cd6773e0913bb80e78d1a97098e860778301
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2022-02-20 21:12:01 +0330

    logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ app_name='accounts'
 
 urlpatterns = [
     path('login' , login_view , name ='login'),
-    # path('logout' , logout_view , name ='logout'),
+    path('logout' , logout_view , name ='logout'),
     path('signup' , signup_view , name ='signup'),
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render , redirect
 from accounts.urls import *
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -22,8 +23,10 @@ def login_view(request):
     else:
         return redirect('/')
 
-# def logout_view(request):
-#     return render(request, 'accounts/logout.html')
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('/') 
 
 def signup_view(request):
-    return render(request, 'accounts\signup.html')
+    return render(request, 'accounts/signup.html')
